0d2972ce6f29004640aab62068a626efbe8747e2
Mahdi Monajjem <mhd.astro2004@gmail.com>
2025-07-13 13:52:29 +0330

    add orchestrator addres to app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+# Import the CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from api import endpoints
 
 app = FastAPI(
@@ -6,6 +8,28 @@ app = FastAPI(
     description="A service to generate structured product JSON from raw text using the Gemini API.",
     version="1.0.0"
 )
+
+# Define the list of "origins" (domains) that are allowed to make requests.
+# You should replace "http://localhost:3000" and "https://your-frontend-app.com"
+# with the actual domains of your other app.
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "https://127.0.0.1:8000",
+     "product-importer.basalam.ir" # Add the production domain of your frontend app here
+]
+
+
+# Add the CORSMiddleware to your application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 # Include the router from api/endpoints.py
 # This makes the "/generate-description" endpoint available.
